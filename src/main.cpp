262b75cb3387d8c40185fa9bc3535039e60c40fb
剑心 <985637214@qq.com>
2026-05-07 13:51:42 +0800

    整理main

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,22 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#include <micro_ros_platformio.h>
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+#include <rcl/rcl.h>
+#include <rclc/rclc.h>
+#include <rclc/executor.h>
+#include <std_msgs/msg/int32.h>
+
+
+
+
+
+void setup()
+{
+    
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y; 
+void loop()
+{
+delay(1);
 }
